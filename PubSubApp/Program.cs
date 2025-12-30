68c8ad77c8a40af3addf46841211ede3e4a02dfa
digitalxnomad <digitalxnomad@gmail.com>
2025-12-30 12:21:49 -0500

    udpate

--- PubSubApp/Program.cs
+++ PubSubApp/Program.cs
@@ -13,7 +13,7 @@ using System.Xml.Linq;
 
 partial class Program
 {
-    static string Version = "PubSubApp 12/17/25 v1.0.4";
+    static string Version = "PubSubApp 12/29/25 v1.0.4";
 
     static async Task Main(string[] args)
     {
