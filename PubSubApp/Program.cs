ac283a746b33bc68b2645a0bb9e9f79ad5e8e5a0
digitalxnomad <digitalxnomad@gmail.com>
2026-01-05 15:27:22 -0500

    checkin

--- PubSubApp/Program.cs
+++ PubSubApp/Program.cs
@@ -13,7 +13,7 @@ using System.Xml.Linq;
 
 partial class Program
 {
-    static string Version = "PubSubApp 01/02/26 v1.0.6";
+    static string Version = "PubSubApp 01/05/26 v1.0.7";
 
     static async Task Main(string[] args)
     {
