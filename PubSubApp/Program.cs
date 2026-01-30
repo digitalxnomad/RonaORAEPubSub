43e33e9264c7f03332db23fa3b6d940099475ed2
digitalxnomad <digitalxnomad@gmail.com>
2026-01-30 10:50:02 -0500

    Version update 01/30/26 v1.0.34

--- PubSubApp/Program.cs
+++ PubSubApp/Program.cs
@@ -16,7 +16,7 @@ using System.Xml.Linq;
 
 public partial class Program
 {
-    static string Version = "PubSubApp 01/29/26 v1.0.33";
+    static string Version = "PubSubApp 01/30/26 v1.0.34";
 
     public static async Task Main(string[] args)
     {
