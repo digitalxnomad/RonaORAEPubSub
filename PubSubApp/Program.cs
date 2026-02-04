ba881c856dc1ad2f110993f3e2c477a30f233c34
digitalxnomad <digitalxnomad@gmail.com>
2026-02-04 15:14:30 -0500

    PubSubApp 02/04/26 v1.0.44
    SLFTX3 Charged TAX3 Default to N, Y for SKU with HST
    SLFTX4 Charged TAX4 Default to N, Y for SKU with Partial HST

--- PubSubApp/Program.cs
+++ PubSubApp/Program.cs
@@ -16,7 +16,7 @@ using System.Xml.Linq;
 
 public partial class Program
 {
-    static string Version = "PubSubApp 02/04/26 v1.0.43";
+    static string Version = "PubSubApp 02/04/26 v1.0.44";
 
     public static async Task Main(string[] args)
     {
